75815b140897d4619817efb6a63eee6a1dbd8051
alie024 <com.alie02024@gmail.com>
2026-06-12 10:01:06 -0600

    Implementada conexión segura a base de datos usando MySQLi y pruebas de enlace

--- basededatos.sql
+++ basededatos.sql
@@ -150,3 +150,9 @@ FROM productos p
 INNER JOIN categorias c ON p.categoria_id = c.id
 GROUP BY c.nombre_categoria;
 
+<?php
+// Configuración de las credenciales de la base de datos
+$host = "localhost";
+$db_name = "sistema_inventario";
+$username = "root";
+$password = ""; // Vacío por defecto en XAMPPgi
